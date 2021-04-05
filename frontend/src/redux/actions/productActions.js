2c71c34200819f8b0053f1668ff2bd224eb3450a
Harshil <gambhirsahab3@gmail.com>
2021-04-05 14:32:41 +0530

    heroku

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -5,7 +5,7 @@ export const getProducts = () => async (dispatch) => {
   try {
     dispatch({ type: actionTypes.GET_PRODUCTS_REQUEST });
 
-    const { data } = await axios.get("/api/products");
+    const { data } = await axios.get("https://sleepy-crag-69552.herokuapp.com/api/products");
 
     dispatch({
       type: actionTypes.GET_PRODUCTS_SUCCESS,
@@ -26,7 +26,7 @@ export const getProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_REQUEST });
 
-    const { data } = await axios.get(`/api/products/${id}`);
+    const { data } = await axios.get(`https://sleepy-crag-69552.herokuapp.com/api/products/${id}`);
 
     dispatch({
       type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS,
