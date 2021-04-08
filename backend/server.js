877d9df3ee1d68e67fe45c720817f44684067591
Harshil <gambhirsahab3@gmail.com>
2021-04-08 21:31:39 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,7 @@ app.get("https://sleepy-crag-69552.herokuapp.com/api/products/", (req, res) => {
   res.json({ message: "API running..." });
 });
 
-app.use("https://sleepy-crag-69552.herokuapp.com/api/products", productRoutes);
-
-{/*app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../frontend/public', 'index.html'));
-});
-*/}
-
-console.log(__dirname);
+app.use("https://sleepy-crag-69552.herokuapp.com/api/products/", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
