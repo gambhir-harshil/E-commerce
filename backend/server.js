0462c87c80740214963d5e31dde8a5535d62d7e6
Harshil <gambhirsahab3@gmail.com>
2021-04-09 08:57:30 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 if (process.env.NODE_ENV === "production"){
   app.use("/", express.static("build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "../frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname,  "../frontend/build", "index.html"));
   });
 }
 
