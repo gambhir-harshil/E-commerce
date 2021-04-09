e9e505ccb419de7b2e52837a7ab3fc6e7b2c8978
Harshil <gambhirsahab3@gmail.com>
2021-04-09 08:12:58 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,14 @@ app.get("/", (req, res) => {
   res.json({ message: "API running..." });
 });
 
-app.use("https://sleepy-crag-69552.herokuapp.com/api/products/", productRoutes);
+if (process.env.NODE_ENV === "production"){
+  app.use("/", express.static("build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "../frontend/build", "index.html"));
+  });
+}
+
+app.use("/api/products/", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
